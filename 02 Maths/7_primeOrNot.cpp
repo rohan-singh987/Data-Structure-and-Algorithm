61a94b64cc29ac333ffb94a7095bfad32f5fa4ca
Rohan Singh <rohannsingh98765@gmail.com>
2021-12-04 18:53:16 +0530

    Adding Basic maths solution
    Signed-off-by: Rohan Singh <rohannsingh98765@gmail.com>

---
+++ 02 Maths/7_primeOrNot.cpp
@@ -0,0 +1,31 @@
+// Prime Number: Number whose only divisior are number itself and 1 
+//                 eg: 2,3,5,7,11,13........etc
+
+
+#include<iostream>
+using namespace std;
+
+bool primeOrNot(int n)
+{
+    if (n==1)
+    {
+        return false;
+    }
+    for (int i = 2; i < n; i++)
+    {
+        if (n % i == 0)
+        {
+            return false;
+        }
+        
+    }
+    return true;
+}
+
+int main(){
+
+    cout<<primeOrNot(7);
+
+
+    return 0;
+}
--- readme.md
+++ readme.md
@@ -5,3 +5,7 @@
 2. Checking weather the no is Palindrome (reverse == original) or not
 3. Calculating Factorial
 4. Counting no of zeros in factorial
+5. GCD of two number
+6. LCM of two number
+7. Prime Or Not
+8. printing all the Prime Factors of a number
